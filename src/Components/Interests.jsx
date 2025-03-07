660589b36aac1544fcfd59be518d83444917a3a1
CodeSandbox <support@codesandbox.io>
2025-03-07 15:13:06 +0000

    Sandbox snapshot commit

--- src/Components/Interests.jsx
+++ src/Components/Interests.jsx
@@ -1,5 +1,49 @@
 const Interests = ({ data, setdata }) => {
-  return <div>Interests</div>;
+  const { interests } = data;
+
+  const handledatachange = (e,name) => {
+    setdata((prevstate) => ({
+      ...prevstate,
+      interests:
+    }));
+  };
+
+  return (
+    <div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            name="coding"
+            checked={interests.includes("coding")}
+          />
+          Coding
+        </label>
+      </div>
+
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            name="insta"
+            checked={interests.includes("insta")}
+          />
+          Insta
+        </label>
+      </div>
+
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            name="chess"
+            checked={interests.includes("chess")}
+          />
+          Chess
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default Interests;
--- src/Components/Settings.jsx
+++ src/Components/Settings.jsx
@@ -1,6 +1,39 @@
-const Settings = () => {
-    return <div>Settings</div>;
+const Settings = ({ data, setdata }) => {
+  const { theme } = data;
+
+  const handledatachange = (e) => {
+    setdata((prevstate) => ({
+      ...prevstate,
+      theme: e.target.name,
+    }));
   };
-  
-  export default Settings;
-  
+
+  return (
+    <div>
+      <div>
+        <label>
+          <input
+            type="radio"
+            name="dark"
+            checked={theme == "dark"}
+            onChange={handledatachange}
+          />
+          Dark
+        </label>
+      </div>
+      <div>
+        <label>
+          <input
+            type="radio"
+            name="light"
+            checked={theme == "light"}
+            onChange={handledatachange}
+          />
+          Light
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
